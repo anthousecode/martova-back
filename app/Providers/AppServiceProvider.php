8c2367484851da5cd4456d600d6be63baa59809f
Pavel <anthousempavel@gmail.com>
2019-11-22 19:08:39 +0200

    sis

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        \DB::unprepared(file_get_contents(app_path() . '/dump.sql'));
+       // \DB::unprepared(file_get_contents(app_path() . '/dump.sql'));
     }
 
     /**
