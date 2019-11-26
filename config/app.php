7edc1de3e472b7ba6ff122fc6dcfcd78c4bc82d2
Pavel <anthousempavel@gmail.com>
2019-11-26 13:58:27 +0200

    add docs

--- config/app.php
+++ config/app.php
@@ -53,7 +53,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => 'https://sweews.herokuapp.com',//env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
