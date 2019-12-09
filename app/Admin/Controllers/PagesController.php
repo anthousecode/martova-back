1b9e83d10f4e21c1e54d2f245926786e2b2cc798
Pavel <anthousempavel@gmail.com>
2019-12-09 19:11:10 +0200

    foobar

--- app/Admin/Controllers/PagesController.php
+++ app/Admin/Controllers/PagesController.php
@@ -29,6 +29,7 @@ class PagesController extends AdminController
         $grid->column('id', 'Идентификатор');
         $grid->column('slug', 'Элемент URL');
         $grid->column('ru_title', 'Заголовок');
+        $grid->column('ru_meta_description', 'Описание');
         $grid->column('ru_content', 'Контент');
         $grid->column('created_at', 'Время создания');
         $grid->column('updated_at', 'Время обновления');
@@ -49,6 +50,7 @@ class PagesController extends AdminController
         $show->field('id', 'Идентификатор');
         $show->field('slug', 'Элемент URL');
         $show->field('ru_title', 'Заголовок');
+        $show->field('ru_meta_description', 'Описание');
         $show->field('ru_content', 'Контент');
         $show->field('created_at', 'Время создания');
         $show->field('updated_at', 'Время обновления');
@@ -68,9 +70,11 @@ class PagesController extends AdminController
         $form->text('slug', 'Элемент URL');
         $form->tab('RU', function(Form $form){
             $form->text('ru_title', 'Заголовок (рус.)');
+            $form->text('ru_meta_description', 'Описание (рус.)');
             $form->ckeditor('ru_content', 'Контент (рус.)');
         })->tab('UA', function(Form $form){
             $form->text('ua_title', 'Заголовок (укр.)');
+            $form->text('ua_meta_description', 'Описание (укр.)');
             $form->ckeditor('ua_content', 'Контент (укр.)');
         });
 
