7d7f11ca13ce071efc6f2fd951310f5bd63dbbcd
Pavel <anthousempavel@gmail.com>
2019-12-09 20:13:58 +0200

    foobar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+
         /*
         Schema::dropIfExists('infrastructures');
         Schema::dropIfExists('news');
