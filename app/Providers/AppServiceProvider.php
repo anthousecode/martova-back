5c040beccfb627ad7a41b640537ee88e26527640
Pavel <anthousempavel@gmail.com>
2019-12-09 19:23:36 +0200

    foobar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,19 +15,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Schema::dropIfExists('infrastructures');
-        Schema::create('infrastructures', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('category_id')->nullable();
-            $table->string('ru_name')->default('-')->nullable();
-            $table->text('ru_description')->default('-')->nullable();
-            $table->string('ua_name')->default('-')->nullable();
-            $table->text('ua_description')->default('-')->nullable();
-            $table->string('image')->nullable();
-            $table->integer('X')->nullable();
-            $table->integer('Y')->nullable();
-            $table->timestamps();
-        });
 
         /*
         Schema::dropIfExists('infrastructures');
