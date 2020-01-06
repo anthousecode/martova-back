fc42a2cfa1dc7d891f5b60b18b3f93654bc64d80
Pavel <anthousempavel@gmail.com>
2020-01-06 14:02:50 +0200

    sos

--- app/Models/News.php
+++ app/Models/News.php
@@ -9,4 +9,9 @@ class News extends Model
     protected $table = "news";
 
     protected $fillable = ['created_at', 'updated_at'];
+
+    public function getUpdatedAtAttribute($date)
+    {
+        return \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('d-m-Y');
+    }
 }
