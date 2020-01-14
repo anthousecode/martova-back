57065edf71e819770f4790c2f363dcb7e16f093b
Pavel <anthousempavel@gmail.com>
2020-01-14 13:43:47 +0200

    basic api auth added via passport

--- routes/api.php
+++ routes/api.php
@@ -14,25 +14,27 @@ use Illuminate\Routing\Router;
 |
 */
 
-Route::post('login', ['uses'=>'API\AuthController@login','as'=>'login']);
-Route::post('register', 'API\AuthController@register');
+Route::namespace('API')->name('api.')->group(function () {
 
-Route::namespace('API')->group(['middleware' => 'auth:api'], function(){
+    Route::post('login', ['uses' => 'AuthController@login', 'as' => 'login']);
+    Route::post('register', 'AuthController@register');
 
-    Route::get('/search-area/{num}', ['uses' => 'AreaController@searchArea', 'as' => 'search_area']);
-    Route::get('/fetch-areas', ['uses' => 'AreaController@fetchAreas', 'as' => 'fetch_areas']);
-    Route::get('/filter-by-status/{status}', ['uses' => 'AreaController@filterByStatus', 'as' => 'filter_by_status']);
-    Route::get('/show-specific/{id}', ['uses' => 'AreaController@show', 'as' => 'show_specific']);
-    Route::get('/download-plan/{id}', ['uses' => 'AreaController@downloadPlan', 'as' => 'download_plan']);
-    Route::get('/download-survey/{id}', ['uses' => 'AreaController@downloadSurvey', 'as' => 'download_survey']);
+    Route::group(['middleware' => ['auth:api']], function() {
+        Route::get('/search-area/{num}', ['uses' => 'AreaController@searchArea', 'as' => 'search_area']);
+        Route::get('/fetch-areas', ['uses' => 'AreaController@fetchAreas', 'as' => 'fetch_areas']);
+        Route::get('/filter-by-status/{status}', ['uses' => 'AreaController@filterByStatus', 'as' => 'filter_by_status']);
+        Route::get('/show-specific/{id}', ['uses' => 'AreaController@show', 'as' => 'show_specific']);
+        Route::get('/download-plan/{id}', ['uses' => 'AreaController@downloadPlan', 'as' => 'download_plan']);
+        Route::get('/download-survey/{id}', ['uses' => 'AreaController@downloadSurvey', 'as' => 'download_survey']);
 
-    Route::get('/pages', ['uses' => 'PagesController@fetchPages']);
-    Route::get('/page/{slug}', ['uses' => 'PagesController@getByUniqueSlug']);
+        Route::get('/pages', ['uses' => 'PagesController@fetchPages']);
+        Route::get('/page/{slug}', ['uses' => 'PagesController@getByUniqueSlug']);
 
-    Route::get('/news', ['uses' => 'NewsController@fetchNews']);
-    Route::get('/infrastructure-items', ['uses' => 'InfrastructureController@fetchInfrastructureItems']);
-    Route::get('/gallery-items', ['uses' => 'GalleryController@fetchGalleryItems']);
+        Route::get('/news', ['uses' => 'NewsController@fetchNews']);
+        Route::get('/infrastructure-items', ['uses' => 'InfrastructureController@fetchInfrastructureItems']);
+        Route::get('/gallery-items', ['uses' => 'GalleryController@fetchGalleryItems']);
 
-    Route::post('logout', 'API\AuthController@logout');
-    Route::post('check', 'API\AuthController@check');
+        Route::post('logout', 'API\AuthController@logout');
+        Route::post('check', 'API\AuthController@check');
+    });
 });
