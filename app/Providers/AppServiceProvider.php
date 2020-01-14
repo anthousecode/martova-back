d56530cd42be35576c903c406c4b70b8ad57340a
Pavel <anthousempavel@gmail.com>
2020-01-14 12:27:15 +0200

    infrestructure changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,12 @@ class AppServiceProvider extends ServiceProvider
     {
        \URL::forceScheme('https');
         $this->app->register(\L5Swagger\L5SwaggerServiceProvider::class);
+
+        Schema::table('infrastructures', function(Blueprint $table){
+            $table->string('icon')->nullable();
+            $table->time('timing')->nullable();
+            $table->string('video')->nullable();
+        });
     }
 
     /**
