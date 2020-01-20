3e435160aa24ff079090b266bbc74d4f7daf2544
Pavel <anthousempavel@gmail.com>
2020-01-20 10:59:15 +0200

    simple auth chech

--- config/services.php
+++ config/services.php
@@ -33,14 +33,14 @@ return [
     'google' => [
         //'key' => 'AIzaSyBGXOW5Ko1SLQ2UELr0CEElWgvQ3BwkyQ0',
         'client_id' => '1084938803888-kh942qstrbka2chdpj57612n2pjldphu.apps.googleusercontent.com',
-        'secret' => 'OteA_LFxYEd8BT6k7qnwAW2E',
+        'client_secret' => 'OteA_LFxYEd8BT6k7qnwAW2E',
         'redirect' => 'http://sweews.herokuapp.com/api/oauth/google/callback',
     ],
 
     'youtube' => [
         //'key' => 'AIzaSyBGXOW5Ko1SLQ2UELr0CEElWgvQ3BwkyQ0',
         'client_id' => '1084938803888-kh942qstrbka2chdpj57612n2pjldphu.apps.googleusercontent.com',
-        'secret' => 'OteA_LFxYEd8BT6k7qnwAW2E',
+        'client_secret' => 'OteA_LFxYEd8BT6k7qnwAW2E',
         'redirect' => 'http://sweews.herokuapp.com/api/oauth/youtube/callback',
     ],
 
