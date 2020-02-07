1772621bb10f87b77351e285adcc3156c1657d7a
Pavel <anthousempavel@gmail.com>
2020-02-07 14:53:49 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -49,6 +49,20 @@ class AppServiceProvider extends ServiceProvider
             });
         }
 
+        \Storage::extend('google', function($app, $config){
+            $client = new Google_Client;
+            $client->setClientId(config('services.google.client_id'));
+            $client->setClientSecret(config('services.google.client_secret'));
+            $client->setRedirectUri(config('services.google.redirect'));
+            $client->setAccessType('offline');
+            $client->setApprovalPrompt('force');
+            $client->refreshToken(config('services.google.refresh_token'));
+
+            $service = new \Google_Service_Drive($client);
+            $adapter = new GoogleDriveAdapter($service, config('services.google.folder_id'));
+            return new \League\Flysystem\Filesystem($adapter);
+        });
+
     }
 
     /**
