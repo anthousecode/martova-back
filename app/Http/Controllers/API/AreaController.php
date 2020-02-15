503d22ca0247a0e3a3bf1f7819a2bfc5a097bb5c
foof <back.end.v.dzencode@gmail.com>
2020-02-16 01:16:49 +0200

    foo

--- app/Http/Controllers/API/AreaController.php
+++ app/Http/Controllers/API/AreaController.php
@@ -106,7 +106,7 @@ class AreaController extends Controller
      */
     public function show($id = null)
     {
-        return AreaResource::collection(Area::where('id', $id)->first());
+        return new AreaResource(Area::where('id', $id)->first());
     }
 
     /**
