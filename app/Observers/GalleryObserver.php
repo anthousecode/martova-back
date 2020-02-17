24cf3a48024bb86f7d2a08e2ea47092c8dbaf370
Pavel <anthousempavel@gmail.com>
2020-02-17 17:28:51 +0200

    foo

---
+++ app/Observers/GalleryObserver.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Observers;
+
+use App\Gallery;
+use App\Services\Cloud\GoogleDrive;
+
+class GalleryObserver
+{
+    protected $googleDrive;
+
+    public function __construct(GoogleDrive $googleDrive)
+    {
+        $this->googleDrive = $googleDrive;
+    }
+
+    /**
+     * Handle the gallery "created" event.
+     *
+     * @param  \App\Gallery  $gallery
+     * @return void
+     */
+    public function created(Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Handle the gallery "updated" event.
+     *
+     * @param  \App\Gallery  $gallery
+     * @return void
+     */
+    public function updated(Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Handle the gallery "deleted" event.
+     *
+     * @param  \App\Gallery  $gallery
+     * @return void
+     */
+    public function deleted(Gallery $gallery)
+    {
+        dd(1);
+    }
+
+    /**
+     * Handle the gallery "restored" event.
+     *
+     * @param  \App\Gallery  $gallery
+     * @return void
+     */
+    public function restored(Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Handle the gallery "force deleted" event.
+     *
+     * @param  \App\Gallery  $gallery
+     * @return void
+     */
+    public function forceDeleted(Gallery $gallery)
+    {
+        //
+    }
+}
