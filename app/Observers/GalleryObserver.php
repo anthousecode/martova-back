a5584c0f873d67344470629ab1bf0027a2d00075
Pavel <anthousempavel@gmail.com>
2020-02-18 15:18:10 +0200

    foo

--- app/Observers/GalleryObserver.php
+++ app/Observers/GalleryObserver.php
@@ -36,9 +36,10 @@ class GalleryObserver
      */
     public function deleted(Gallery $gallery)
     {
-        for ($i=0; $i<10;$i++) {
-            Gallery::create(['image' => 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']);
-        }
+        dd(1);
+       // for ($i=0; $i<10;$i++) {
+       //     Gallery::create(['image' => 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']);
+        //}
     }
 
     /**
