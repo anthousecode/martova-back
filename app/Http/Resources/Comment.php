a581e7701b275aa6d4fef85b4e06dab07650ee70
Алексей Синявский <sinyavskij00@gmail.com>
2020-02-25 13:18:22 +0200

    foo

--- app/Http/Resources/Comment.php
+++ app/Http/Resources/Comment.php
@@ -3,24 +3,34 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 
 class Comment extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
+        $fileType = '';
+        if ($this->image) {
+            if (Str::contains($this->file_type, ['jpg', 'jpeg', 'png'])) {
+                $fileType = 'image';
+            }
+            if (Str::contains($this->file_type, ['mp4', 'avi'])) {
+                $fileType = 'video';
+            }
+        }
         return [
             'id' => $this->id,
             'news' => $this->news,
             'author' => $this->author,
             'text' => $this->text,
             'image' => $this->image ? sprintf("https://drive.google.com/uc?id=%s&export=download", $this->image) : '',
-            'file_type' => $this->image ? ($this->file_type ? (!preg_match('/mp4/', $this->file_type) ? 'img' : 'video') : '') : '',
+            'file_type' => $fileType,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
