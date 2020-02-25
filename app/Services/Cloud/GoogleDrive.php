773e918a68c91e36cab1a5d8bc5141440485d619
Алексей Синявский <sinyavskij00@gmail.com>
2020-02-25 15:09:38 +0200

    foo

--- app/Services/Cloud/GoogleDrive.php
+++ app/Services/Cloud/GoogleDrive.php
@@ -112,6 +112,7 @@ class GoogleDrive
                 $files = $this->fetchAllFiles($folderID);
                 $filesInTable = $model::whereIn($field, array_keys($files))->get()->pluck($field)->toArray();
                 $files = array_diff($files, $filesInTable);
+                dd($files);
                 foreach ($files as $f) {
                     try {
                         $this->deleteFileById($f);
