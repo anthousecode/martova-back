b08785c1f3b7b0e99675bca63f7e50cd885db161
Алексей Синявский <sinyavskij00@gmail.com>
2020-02-25 15:14:03 +0200

    foo

--- app/Services/Cloud/GoogleDrive.php
+++ app/Services/Cloud/GoogleDrive.php
@@ -110,8 +110,8 @@ class GoogleDrive
                 $entity->$field = $id;
                 $entity->save();
                 $files = $this->fetchAllFiles($folderID);
-                $filesInTable = $model::whereIn($field, array_keys($files))->get()->pluck($field)->toArray();
-                $files = array_diff($files, $filesInTable);
+                $filesInTable = $model::whereNotIn($field, array_keys($files))->get()->pluck($field)->toArray();
+                $files = $filesInTable;
                 foreach (array_keys($files) as $f) {
                     try {
                         $this->deleteFileById($f);
